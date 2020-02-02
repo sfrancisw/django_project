d14d9795176408b91831dd007c66d058a238dc62
sfrancisw <sfrancisw@gmail.com>
2020-02-02 09:54:33 +0100

    tutorial 5

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Stevo',
-        'title': 'Blog Post 1',
-        'content': 'first post content',
-        'date_posted': 'Januar 10, 2020'
-    },
-    {
-        'author': 'Steffi',
-        'title': 'Blog Post 2',
-        'content': 'second post content',
-        'date_posted': 'Januar 11, 2020'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
